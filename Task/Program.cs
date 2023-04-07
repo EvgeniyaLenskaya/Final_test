29b65aa96fa9ce3145f6d000e3aa66a7ca1d6545
EvgeniyaLenskaya <janie2010@mail.ru>
2023-04-07 21:11:11 +0800

    add task folder

---
+++ Task/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] firstArray = new string[9] {"Say", "hi", "Hello ", "to", "world ","a", "new", "day", ")"};
+string[] secondArray = new string[firstArray.Length];
+int secondArrayIndex = 0;
+
+string[] resultArray = FillResultArray(firstArray, secondArray);
+string[] FillResultArray(string[] firstArray, string[] secondArray)
+{
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if(firstArray[i].Length <=3)
+        {
+            secondArray[secondArrayIndex] = firstArray[i];
+            secondArrayIndex++;
+        }
+    }
+    return secondArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine(); 
+}
+Console.WriteLine();
+PrintArray(firstArray);
+Console.WriteLine();
+PrintArray(secondArray);
